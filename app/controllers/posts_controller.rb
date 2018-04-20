601af8b263c42a7ec1646584396cc008ef10f018
dickanirwansyah <dickanirwansyah@gmail.com>
2018-04-20 20:14:49 +0700

    controller posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def show
@@ -17,8 +17,11 @@ class PostsController < ApplicationController
     #render plain: params[:post].inspect
     @post = Post.new (post_params)
 
-    @post.save
-    redirect_to @post
+    if(@post.save)
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private def post_params
